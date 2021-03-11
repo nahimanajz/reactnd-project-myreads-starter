02de5ab10d045ed91b7c3d9b33cb448582fbfb78
nahimanajz <jazzonahimana@gmail.com>
2021-03-11 21:39:06 +0200

    show book for changing its shelf

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,9 +19,9 @@ render() {
                 <div className="book-top">
                     
                     <div className="book-cover"                                 
-                    style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail || 'nothing'})` }}></div>
                     <div className="book-shelf-changer">
-                    <select onChange={(e)=>this.changeShelfHandler(e.target.value, book.id)}>
+                    <select onChange={(e)=>this.changeShelfHandler(e.target.value, book)}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
